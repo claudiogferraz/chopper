e82a215c9b6ce4602866ece16a805fe49a2ebd03
Claudio Gofe <gofeclaudio@gmail.com>
2023-11-25 08:13:51 -0300

    Fixed date grabbing

--- src/utils/days.ts
+++ src/utils/days.ts
@@ -7,11 +7,7 @@ const getToday = () => dayjs();
 
 const getLastSaturday = () => {
 	let today = getToday();
-	if (today.weekday() == 6) {
-		return today;
-	} else {
-		return today.subtract(1, "w").weekday(6);
-	}
+	return today.subtract(1, "w").weekday(6);
 };
 
 export { getToday, getLastSaturday };
